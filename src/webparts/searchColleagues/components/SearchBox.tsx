64797ed9026a9049eab6a944f0fa34e31f4e373a
SebastianChristobal <sebastiang.pavez@gmail.com>
2024-04-17 16:36:19 +0200

    css updates

--- src/webparts/searchColleagues/components/SearchBox.tsx
+++ src/webparts/searchColleagues/components/SearchBox.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-
+import styles from './SearchColleagues.module.scss';
 import { SearchBox } from '@fluentui/react/lib/SearchBox';
 import { Stack, IStackTokens } from '@fluentui/react/lib/Stack';
 import { ISearchColleaguesProps } from './ISearchColleaguesProps';
 const stackTokens: Partial<IStackTokens> = { childrenGap: 20};
 
 export const ColleaguesSearchBox: React.FC<ISearchColleaguesProps> = ({ onSearch }) => {
-  
+  const [isFocused, setIsFocused] = React.useState(false);
   const handleChange = (newValue: any): void => {
     if (onSearch) {
       onSearch(newValue);
@@ -21,9 +21,11 @@ export const ColleaguesSearchBox: React.FC<ISearchColleaguesProps> = ({ onSearch
         }
       }}
     >
-      <SearchBox
+      
+      <SearchBox  className={isFocused ? styles.searchBoxActive : styles.searchBox} // Apply the active or inactive class based on isFocused state
         placeholder="search for colleagues"
         onChange={(event) => handleChange(event?.target.value)}
+        onFocus={() => setIsFocused(true)}
       />
     </Stack>
   );
